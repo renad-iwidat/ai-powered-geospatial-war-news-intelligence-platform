80ea76204ee3f92ae79dafb18c06a26812f720f4
renad-iwidat <renadiwidat2003@gmail.com>
2026-03-05 23:30:48 +0200

    Remove devIndicators config - not supported in Next.js 16

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -6,10 +6,6 @@ const withNextIntl = createNextIntlPlugin('./i18n/request.ts');
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'standalone', // For Docker deployment
-  devIndicators: {
-    buildActivity: false, // Disable build activity indicator
-    buildActivityPosition: 'bottom-right',
-  },
 };
 
 export default withNextIntl(nextConfig);
